82562291f180d5ee6f60e95177f7bf497b813aac
DefeatedNamer <pedrojbspires@gmail.com>
2021-01-24 00:01:04 +0000

    serie 2

---
+++ Autores.md
@@ -0,0 +1,5 @@
+Trabalho realizado por:
+
+Grupo 14
+43601 - Diogo Santos (Firight)
+44358 - Pedro Pires (DefeatedNamer)
---
+++ Ex2/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    public class Main<R,T>
+    {
+        public async Task<R> Compute(T[] elems, R initial)
+        {
+            R acc = initial;
+
+            foreach (var elem in elems)
+            {
+                T t1 = await A(elem);
+
+                T t2 = await B(t1);
+
+                acc = await C(t2, acc);
+            }
+            return acc;
+        }
+    }
+}
